640c8e5b1a3002b37f9125ae1fbcb49868430ce1
RoshanPatil17 <roshanpatil1705@gmail.com>
2025-06-11 05:22:08 +0000

    js in progress

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,34 @@
+const name="roshan"
+const repoCount=12
+
+//console.log(name+ repoCount);
+//or
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('Roshantara')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt('4'));
+console.log(gameName.indexOf('t'));
+console.log(gameName.substring(0,5));
+console.log(gameName.slice(-6,2));
+
+const newStringOne="   Rosh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url='https://www.youtube.com/watch?v=fozwNnFunlo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=11'
+console.log(url.replace('.com','.in'));
+console.log(url.includes('youtube'));
+
+const fire='rosh-patil-antara'
+console.log(fire.split('-'));
+
+
+
+
+
+
+
